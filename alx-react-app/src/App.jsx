46d3311bc97d6e219f732183bf9430abc62bb0c5
Henok <hdbeleive@gmail.com>
2025-02-10 18:51:01 +0300

    Props passed

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -26,6 +27,8 @@ function App() {
       <MainContent />
       <Footer />
       <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
+      <UserProfile name="Dobby" age="20" bio="Loves making movies"/>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
