dbbc6e6e16b99f54a108010aff3ad6cddcee9890
Henok <hdbeleive@gmail.com>
2025-02-18 14:16:45 +0300

    styling done

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer style={{ backgroundColor: 'white', color: 'black', textAlign: 'center', fontStyle:'italic', display:'block', padding: '8px' }}>
       <p>© 2023 City Lovers</p>
     </footer>
   );
