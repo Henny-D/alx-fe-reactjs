66246edd791a2d4a2345f5be98372c574e9d7ce3
Henok <hdbeleive@gmail.com>
2025-02-18 14:18:51 +0300

    correction

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 export default function MainContent(){
     return(
 <main style={{ backgroundColor: 'white', color: 'purple', textAlign: 'center', padding:'10px'}}>
-  <p>I love to visit <strong>New York, Paris, and Tokyo</strong>.</p>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
 </main>
     );
 }
