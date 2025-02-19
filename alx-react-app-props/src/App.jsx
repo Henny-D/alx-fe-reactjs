70ac3611b28c3a01f24e95e35b3e89eb7ab16d83
Henok <hdbeleive@gmail.com>
2025-02-19 15:09:06 +0300

    my company

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,12 +3,14 @@ import UserContext from './components/UserContext';
 
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com"};
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com"}
 
   return (
+    <div style={{ backgroundColor: 'white', color: 'black', textAlign: 'center', padding:'5%', marginLeft:'500px'}}>
   <UserContext.Provider value={userData}>
   <ProfilePage />
   </UserContext.Provider>
+  </div>
   );
 }
 
