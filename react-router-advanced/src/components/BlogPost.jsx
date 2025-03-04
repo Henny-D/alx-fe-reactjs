60259025f53a1702c5d0de212bc51d30776af2e7
Henok <hdbeleive@gmail.com>
2025-03-04 19:58:53 +0300

    id corrected

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,12 +1,12 @@
 import { useParams } from "react-router-dom";
 
 function BlogPost() {
-  let { Id } = useParams();
+  let { id } = useParams();
 
   return (
     <div>
-      <h1>Blog Post {Id}</h1>
-      <p>This is the content for blog post {Id}.</p>
+      <h1>Blog Post {id}</h1>
+      <p>This is the content for blog post {id}.</p>
     </div>
   );
 }
