f8bc3b6316c2334b2be4632407fbc452a8e9bc2f
Henok <hdbeleive@gmail.com>
2025-03-18 09:30:24 +0300

    react app setup and home page development

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+import './index.css'
+import HomePage from './components/HomePage'
+
+function App() {
+  
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
