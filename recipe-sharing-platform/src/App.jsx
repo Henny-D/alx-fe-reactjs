5d6aae963328c4842197acb9e24ef240bb5dc2d0
Henok <hdbeleive@gmail.com>
2025-03-18 11:20:15 +0300

    Home page created and json store developed

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './index.css'
+import HomePage from './components/HomePage'
 
 function App() {
   
   return (
     <>
       
-      <h1 className='text-blue-500'>Vite + React</h1>
+      <HomePage />
       
     </>
   )
